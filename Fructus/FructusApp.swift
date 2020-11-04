ba27cc6172e4084f6c7f3018d18bf54271260029
Yermek Sabyrzhan <Tisobyn@users.noreply.github.com>
2020-11-04 22:25:11 +0600

    FirstCommit
    firstCommit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Yermek Sabyrzhan on 04.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
